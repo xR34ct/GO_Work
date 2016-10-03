b33de3ba488b1387b5917c2cff2b71deeaed8391
xR34ct <avdagic94@gmail.com>
2016-10-03 16:58:12 +0200

    Pascals Triangel working

--- pascal.go
+++ pascal.go
@@ -5,7 +5,7 @@ import (
 
 func main() {
 
-	var row, place, number int
+	var row, place int64
 	 
     fmt.Print("Enter row: ")
     fmt.Scan(&row)
@@ -13,17 +13,22 @@ func main() {
     fmt.Print("Enter place: ")
     fmt.Scan(&place)
 
-    number = PascalCalc(row,place)
+    fmt.Println(" ")
 
-    fmt.Println("The number is: ")
-    fmt.Println(number)
+    for place > row { //If place > row loop
+    	fmt.Println("Place cannot be bigger than row!")
+    	fmt.Println(" ")
+    	fmt.Println("Enter place: ")
+    	fmt.Scan(&place)
+    }
 
+    fmt.Print("The number is: ")
+    fmt.Println(PascalCalc(row,place))
 }
 
-func PascalCalc(r int, p int) (int) {
+func PascalCalc(r, p int64) (int64) { //Recursive function to calculate the number in Pascal's triangle
 
 	if r == 1 || r == 2 || p == r || p == 1 {
-
 		return 1
 	} else {
 			return (PascalCalc(r-1,p-1)+PascalCalc(r-1,p))
