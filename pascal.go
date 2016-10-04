f4a415651d3687acecb80e6e8036beb1ed7a9ed9
xR34ct <avdagic94@gmail.com>
2016-10-04 18:31:59 +0200

    Boundries

--- pascal.go
+++ pascal.go
@@ -1,36 +1,55 @@
 package main
+
 import (
-    "fmt"
+	"fmt"
+	"time"
 )
 
 func main() {
 
-	var row, place int64
-	 
-    fmt.Print("Enter row: ")
-    fmt.Scan(&row)
+	var row, place int
 
-    fmt.Print("Enter place: ")
-    fmt.Scan(&place)
+	fmt.Print("Enter row: ")
+	fmt.Scan(&row)
 
-    fmt.Println(" ")
+	for row < 1 {
+		fmt.Println("Row cannot be lesser than 0!")
+		fmt.Println(" ")
+		fmt.Print("Enter row: ")
+		fmt.Scan(&place)
+	}
 
-    for place > row { //If place > row loop
-    	fmt.Println("Place cannot be bigger than row!")
-    	fmt.Println(" ")
-    	fmt.Println("Enter place: ")
-    	fmt.Scan(&place)
-    }
+	fmt.Print("Enter place: ")
+	fmt.Scan(&place)
 
-    fmt.Print("The number is: ")
-    fmt.Println(PascalCalc(row,place))
+	for place < 0 {
+		fmt.Println("Place cannot be lesser than 0!")
+		fmt.Println(" ")
+		fmt.Print("Enter place: ")
+		fmt.Scan(&place)
+	}
+
+	fmt.Println(" ")
+
+	for place > row { //If place > row loop
+		fmt.Println("Place cannot be bigger than row!")
+		fmt.Println(" ")
+		fmt.Print("Enter place: ")
+		fmt.Scan(&place)
+	}
+
+	fmt.Print("The number is: ")
+	start := time.Now()
+	fmt.Println(PascalCalc(row, place))
+	elapsed := time.Since(start)
+	fmt.Println("Såll", elapsed)
 }
 
-func PascalCalc(r, p int64) (int64) { //Recursive function to calculate the number in Pascal's triangle
+func PascalCalc(r, p int) int64 { //Recursive function to calculate the number in Pascal's triangle
 
 	if r == 1 || r == 2 || p == r || p == 1 {
 		return 1
 	} else {
-			return (PascalCalc(r-1,p-1)+PascalCalc(r-1,p))
-		}
+		return (PascalCalc(r-1, p-1) + PascalCalc(r-1, p))
+	}
 }
